47a7faa2b92c177fca801528f8bed4c1c141e5b3
Benjamim Pinto <bpinto@trt13.jus.br>
2022-06-14 00:17:34 -0300

    Add 'npm run report' script

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,14 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   projectId: 'j2smct',
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+  trashAssetsBeforeRuns: true,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
@@ -9,14 +17,5 @@ module.exports = defineConfig({
       return require('./cypress/plugins/index.js')(on, config)
     },
     experimentalSessionAndOrigin: true,
-  },
-
-  // Reporter config
-  reporter: 'mochawesome',
-  reporterOptions: {
-    reportDir: 'cypress/results',
-    overwrite: false,
-    html: false,
-    json: true,
   },
 })
